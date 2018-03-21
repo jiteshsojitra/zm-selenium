e24ed7c6e31066fda2184a8acdc0e7b7c5df15a2
Jitesh Sojitra <jitesh.sojitra@synacor.com>
2018-03-21 15:27:50 +0530

    Fixing path in docker step

--- .circleci/install-prerequisites.sh
+++ .circleci/install-prerequisites.sh
@@ -27,19 +27,18 @@ apt-get -qq install -y apt-utils \
 
 # Java
 echo "===> add webupd8 repository..."  && \
-echo "deb http://ppa.launchpad.net/webupd8team/java/ubuntu trusty main" | tee /etc/apt/sources.list.d/webupd8team-java.list  && \
-echo "deb-src http://ppa.launchpad.net/webupd8team/java/ubuntu trusty main" | tee -a /etc/apt/sources.list.d/webupd8team-java.list  && \
-apt-key adv --keyserver keyserver.ubuntu.com --recv-keys EEA14886  && \
-apt-get update  && \
-echo "===> install Java"  && \
-echo debconf shared/accepted-oracle-license-v1-1 select true | debconf-set-selections  && \
-echo debconf shared/accepted-oracle-license-v1-1 seen true | debconf-set-selections  && \
-DEBIAN_FRONTEND=noninteractive  apt-get install -y --force-yes oracle-java8-installer oracle-java8-set-default
+	echo "deb http://ppa.launchpad.net/webupd8team/java/ubuntu trusty main" | tee /etc/apt/sources.list.d/webupd8team-java.list  && \
+	echo "deb-src http://ppa.launchpad.net/webupd8team/java/ubuntu trusty main" | tee -a /etc/apt/sources.list.d/webupd8team-java.list  && \
+	apt-key adv --keyserver keyserver.ubuntu.com --recv-keys EEA14886  && \
+	apt-get update  && \
+	echo "===> install Java"  && \
+	echo debconf shared/accepted-oracle-license-v1-1 select true | debconf-set-selections  && \
+	echo debconf shared/accepted-oracle-license-v1-1 seen true | debconf-set-selections  && \
+	DEBIAN_FRONTEND=noninteractive  apt-get install -y --force-yes oracle-java8-installer oracle-java8-set-default
 
 # Chrome
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 dpkg -i google-chrome-stable_current_amd64.deb; apt-get -fy install
 
 # Ant contrib
-cd ~/.zcs-deps
-wget https://files.zimbra.com/repository/ant-contrib/ant-contrib-1.0b1.jar
+cd ~/.zcs-deps && wget https://files.zimbra.com/repository/ant-contrib/ant-contrib-1.0b1.jar
