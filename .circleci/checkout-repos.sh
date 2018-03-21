9820a45a28819f035a1e5dcd8399c1d987be73cf
Jitesh Sojitra <jitesh.sojitra@synacor.com>
2018-03-22 00:39:51 +0530

    Adding sample tests

--- .circleci/checkout-repos.sh
+++ .circleci/checkout-repos.sh
@@ -9,3 +9,6 @@ git clone --depth=1 -b $CIRCLE_BRANCH https://github.com/Zimbra/zm-mailbox.git ~
 git clone --depth=1 -b $CIRCLE_BRANCH https://github.com/Zimbra/zm-ajax.git ~/zm-ajax
 git clone --depth=1 -b $CIRCLE_BRANCH https://github.com/Zimbra/zm-web-client.git ~/zm-web-client
 git clone --depth=1 -b $CIRCLE_BRANCH https://github.com/Zimbra/zm-zimlets.git ~/zm-zimlets
+
+firefox
+curl 'http://localhost:6901/?password=vncpassword'
