2cd964926d4034ef7f464f9542e71328ca0be5fa
Jitesh Sojitra <jitesh.sojitra@synacor.com>
2018-03-21 12:18:28 +0530

    Fixing path in docker step

--- .circleci/prepare-box.sh
+++ .circleci/prepare-box.sh
@@ -11,10 +11,8 @@ mkdir -p ~/.ivy2/cache
 export DEBIAN_FRONTEND=noninteractive
 export GIT_BRANCH=develop
 export JAVA_HOME=/usr/lib/jvm/java-8-oracle
-export ROOT_DIR=/root
 
 # Update the repository sources list
-cd $ROOT_DIR
 apt-get -qq -y autoremove
 apt-get -qq -y update
 
@@ -34,14 +32,14 @@ apt-get -qq install -y ant ant-optional ant-contrib
 apt-get -qq install -y firefox
 
 # Java
-RUN apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys EEA14886
-RUN add-apt-repository ppa:webupd8team/java
-RUN apt-get update ; echo oracle-java8-installer shared/accepted-oracle-license-v1-1 select true | sudo /usr/bin/debconf-set-selections
-RUN apt-get install -y oracle-java8-installer oracle-java8-set-default
+apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys EEA14886
+add-apt-repository ppa:webupd8team/java
+apt-get update ; echo oracle-java8-installer shared/accepted-oracle-license-v1-1 select true | sudo /usr/bin/debconf-set-selections
+apt-get install -y oracle-java8-installer oracle-java8-set-default
 
 # Chrome
-RUN wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-RUN dpkg -i google-chrome-stable_current_amd64.deb; apt-get -fy install
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+dpkg -i google-chrome-stable_current_amd64.deb; apt-get -fy install
 
 # Ant contrib
 cd ~/.zcs-deps
