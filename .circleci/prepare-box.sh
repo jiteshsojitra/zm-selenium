b9000d559208f818e364cbdc9705ed0e5156d1e5
Jitesh Sojitra <jitesh.sojitra@synacor.com>
2018-03-21 13:29:02 +0530

    Fixing path in docker step

--- .circleci/prepare-box.sh
+++ .circleci/prepare-box.sh
@@ -17,25 +17,28 @@ apt-get -qq -y autoremove
 apt-get -qq -y update
 
 # Install pre-requisites
-apt-get -qq install -y git
-apt-get -qq install -y wget
-apt-get -qq install -y apt-utils
-apt-get -qq install -y software-properties-common
-apt-get -qq install -y build-essential
-apt-get -qq install -y sudo
-apt-get -qq install -y iputils-ping
-apt-get -qq install -y openssh-server
-apt-get -qq install -y vim
-apt-get -qq install -y wget
-apt-get -qq install -y git
-apt-get -qq install -y ant ant-optional ant-contrib
-apt-get -qq install -y firefox
+apt-get -qq install -y apt-utils \
+	xdg-utils \
+	software-properties-common \
+	build-essential \
+	vim \
+	ant ant-optional ant-contrib \
+	openssh-server \
+	wget \
+	iputils-ping \
+	git \
+	firefox
 
 # Java
-apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys EEA14886
-add-apt-repository ppa:webupd8team/java
-apt-get update ; echo oracle-java8-installer shared/accepted-oracle-license-v1-1 select true | sudo /usr/bin/debconf-set-selections
-apt-get install -y oracle-java8-installer oracle-java8-set-default
+echo "===> add webupd8 repository..."  && \
+echo "deb http://ppa.launchpad.net/webupd8team/java/ubuntu trusty main" | tee /etc/apt/sources.list.d/webupd8team-java.list  && \
+echo "deb-src http://ppa.launchpad.net/webupd8team/java/ubuntu trusty main" | tee -a /etc/apt/sources.list.d/webupd8team-java.list  && \
+apt-key adv --keyserver keyserver.ubuntu.com --recv-keys EEA14886  && \
+apt-get update  && \
+echo "===> install Java"  && \
+echo debconf shared/accepted-oracle-license-v1-1 select true | debconf-set-selections  && \
+echo debconf shared/accepted-oracle-license-v1-1 seen true | debconf-set-selections  && \
+DEBIAN_FRONTEND=noninteractive  apt-get install -y --force-yes oracle-java8-installer oracle-java8-set-default
 
 # Chrome
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
