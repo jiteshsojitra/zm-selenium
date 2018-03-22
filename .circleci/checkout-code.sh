6bf4c1fe9f477bcb6f859b148de953a627c39b57
Jitesh Sojitra <jitesh.sojitra@synacor.com>
2018-03-22 11:54:18 +0530

    Display issue check1

--- .circleci/checkout-code.sh
+++ .circleci/checkout-code.sh
@@ -2,6 +2,7 @@
 
 set -euxo pipefail
 
+# Dependent repositories
 git clone --depth=1 -b $CIRCLE_BRANCH https://github.com/Zimbra/zm-mailbox.git ~/zm-mailbox
 git clone --depth=1 -b $CIRCLE_BRANCH https://github.com/Zimbra/zm-ajax.git ~/zm-ajax
 git clone --depth=1 -b $CIRCLE_BRANCH https://github.com/Zimbra/zm-web-client.git ~/zm-web-client
