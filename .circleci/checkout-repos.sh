5dcdd8ce0eb46a1e5871435ac3396b8261cc4c1f
Jitesh Sojitra <jitesh.sojitra@synacor.com>
2018-03-22 09:59:27 +0530

    Adding sample tests

--- .circleci/checkout-repos.sh
+++ .circleci/checkout-repos.sh
@@ -7,13 +7,12 @@ export DEBIAN_FRONTEND=noninteractive
 
 apt-get update
 apt-get install xvfb
-xhost +
-export DISPLAY=:0.0
 Xvfb :1 -screen 0 1024x768x16 &> xvfb.log  &
 
 #startx
 xdg-open https://www.google.com
 #xdg-open http://127.0.0.1:6901/?password=vncpassword
+DISPLAY=:1 firefox
 
 git clone --depth=1 -b $CIRCLE_BRANCH https://github.com/Zimbra/zm-mailbox.git ~/zm-mailbox
 git clone --depth=1 -b $CIRCLE_BRANCH https://github.com/Zimbra/zm-ajax.git ~/zm-ajax
