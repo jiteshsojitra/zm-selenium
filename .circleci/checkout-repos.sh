959d54735a5195c1f83596efa7244a85cff4098a
Jitesh Sojitra <jitesh.sojitra@synacor.com>
2018-03-22 09:44:56 +0530

    Adding sample tests

--- .circleci/checkout-repos.sh
+++ .circleci/checkout-repos.sh
@@ -6,9 +6,7 @@ set -euxo pipefail
 export DEBIAN_FRONTEND=noninteractive
 
 #startx
-Xvfb :0 -screen 0 800x6000x24 +extension GLX -nolisten tcp -dpi 96
-export DISPLAY=:0
-xdg-open http://www.google.com
+xdg-open http://127.0.0.1:6901/?password=vncpassword
 exit;
 
 #xdg-open http://127.0.0.1:6901/?password=vncpassword
