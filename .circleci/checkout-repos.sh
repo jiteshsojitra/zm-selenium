f4db6a373535944dbc7ef8bf95c3dafcc2706e45
Jitesh Sojitra <jitesh.sojitra@synacor.com>
2018-03-22 11:49:19 +0530

    Display issue check1

--- .circleci/checkout-repos.sh
+++ .circleci/checkout-repos.sh
@@ -2,12 +2,6 @@
 
 set -euxo pipefail
 
-firefox &
-#startx
-xdg-open https://www.google.com
-#xdg-open http://127.0.0.1:6901/?password=vncpassword
-
-
 git clone --depth=1 -b $CIRCLE_BRANCH https://github.com/Zimbra/zm-mailbox.git ~/zm-mailbox
 git clone --depth=1 -b $CIRCLE_BRANCH https://github.com/Zimbra/zm-ajax.git ~/zm-ajax
 git clone --depth=1 -b $CIRCLE_BRANCH https://github.com/Zimbra/zm-web-client.git ~/zm-web-client
