f4d28c45779ddf9d6078c691a65fb2ea4ccfa93c
Jitesh Sojitra <jitesh.sojitra@synacor.com>
2018-03-31 19:29:21 +0530

    Adding network selenium repository

--- .circleci/checkout-repos.sh
+++ .circleci/checkout-repos.sh
@@ -7,3 +7,4 @@ git clone --depth=1 -b $CIRCLE_BRANCH https://github.com/Zimbra/zm-mailbox.git ~
 git clone --depth=1 -b $CIRCLE_BRANCH https://github.com/Zimbra/zm-ajax.git ~/zm-ajax
 git clone --depth=1 -b $CIRCLE_BRANCH https://github.com/Zimbra/zm-web-client.git ~/zm-web-client
 git clone --depth=1 -b $CIRCLE_BRANCH https://github.com/Zimbra/zm-zimlets.git ~/zm-zimlets
+git clone --depth=1 -b $CIRCLE_BRANCH https://github.com/Zimbra/zm-network-selenium.git ~/zm-network-selenium
