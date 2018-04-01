20bc52218701b7f2ca21df7b61587c0cc0601df8
Jitesh Sojitra <jitesh.sojitra@synacor.com>
2018-04-01 12:24:10 +0530

    Bringing back environment variables

--- .circleci/checkout-repos.sh
+++ .circleci/checkout-repos.sh
@@ -2,16 +2,13 @@
 
 set -euxo pipefail
 
+# Additionally get circleci container public ip address
+public_ip_address=$(wget -qO- http://checkip.amazonaws.com)
+echo $public_ip_address
+
 # Checkout dependent repositories
 git clone --depth=1 -b $CIRCLE_BRANCH https://github.com/Zimbra/zm-mailbox.git ~/zm-mailbox
 git clone --depth=1 -b $CIRCLE_BRANCH https://github.com/Zimbra/zm-ajax.git ~/zm-ajax
 git clone --depth=1 -b $CIRCLE_BRANCH https://github.com/Zimbra/zm-web-client.git ~/zm-web-client
 git clone --depth=1 -b $CIRCLE_BRANCH https://github.com/Zimbra/zm-zimlets.git ~/zm-zimlets
 git clone --depth=1 -b $CIRCLE_BRANCH https://github.com/Zimbra/zm-network-selenium.git ~/zm-network-selenium
-
-# Get circleci container public ip address
-public_ip_address=$(wget -qO- http://checkip.amazonaws.com)
-echo $public_ip_address
-
-echo $SELENIUM_BROWSER
-echo $SELENIUM_SERVER_HOST
