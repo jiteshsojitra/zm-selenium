3bf8ad7574e773bd6557a4e3e152318bb0e03ed6
Jitesh Sojitra <jitesh.sojitra@synacor.com>
2018-04-07 00:35:29 +0530

    Renamed github repo name

--- .circleci/checkout-repos.sh
+++ .circleci/checkout-repos.sh
@@ -7,8 +7,8 @@ public_ip_address=$(wget -qO- http://checkip.amazonaws.com)
 echo $public_ip_address
 
 # Checkout dependent repositories
-git clone --depth=1 -b $CIRCLE_BRANCH https://github.com/Zimbra/zm-mailbox
-git clone --depth=1 -b $CIRCLE_BRANCH https://github.com/Zimbra/zm-ajax
-git clone --depth=1 -b $CIRCLE_BRANCH https://github.com/Zimbra/zm-web-client
-git clone --depth=1 -b $CIRCLE_BRANCH https://github.com/Zimbra/zm-zimlets
-git clone --depth=1 -b $CIRCLE_BRANCH https://github.com/Zimbra/zm-network-selenium
+git clone --depth=1 -b $CIRCLE_BRANCH https://github.com/Zimbra/zm-mailbox.git
+git clone --depth=1 -b $CIRCLE_BRANCH https://github.com/Zimbra/zm-ajax.git
+git clone --depth=1 -b $CIRCLE_BRANCH https://github.com/Zimbra/zm-web-client.git
+git clone --depth=1 -b $CIRCLE_BRANCH https://github.com/Zimbra/zm-zimlets.git
+git clone --depth=1 -b $CIRCLE_BRANCH https://github.com/Zimbra/zm-network-selenium.git
