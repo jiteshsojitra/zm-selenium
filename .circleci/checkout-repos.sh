4ff490f9182c6e82c2b03f61858cdf05ad0441f2
Jitesh Sojitra <jitesh.sojitra@synacor.com>
2018-04-09 15:20:42 +0530

    Using environment variables from config

--- .circleci/checkout-repos.sh
+++ .circleci/checkout-repos.sh
@@ -2,9 +2,6 @@
 
 set -euxo pipefail
 
-echo $GITHUB_MASTER_BRANCH
-exit;
-
 # Additionally get circleci container public ip address
 CONTAINER_IP_ADDRESS=$(wget -qO- http://checkip.amazonaws.com)
 echo $CONTAINER_IP_ADDRESS
@@ -13,5 +10,5 @@ echo $CONTAINER_IP_ADDRESS
 GITHUB_ORG_URL="https://github.com/Zimbra"
 GITHUB_REPOS=( "zm-mailbox" "zm-ajax" "zm-web-client" "zm-zimlets" "zm-network-selenium" )
 for GITHUB_REPO in "${GITHUB_REPOS[@]}"; do
-	git clone --depth=1 -b $CIRCLE_BRANCH $GITHUB_ORG_URL/$GITHUB_REPO ~/$GITHUB_REPO
+	git clone --depth=1 -b $ENV_BRANCH $GITHUB_ORG_URL/$GITHUB_REPO ~/$GITHUB_REPO
 done
