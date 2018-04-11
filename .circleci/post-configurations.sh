e5847ec9209e5e5273b8f36e9ca176dc715b5be1
Jitesh Sojitra <jitesh.sojitra@synacor.com>
2018-04-11 22:02:47 +0530

    Print top process output

--- .circleci/post-configurations.sh
+++ .circleci/post-configurations.sh
@@ -5,3 +5,6 @@ set -euxo pipefail
 # Create test output links
 ln -s ~/zm-selenium/test-output/*/*/*/*/TestNG/index.html ~/zm-selenium/test-output/report.html
 ln -s ~/zm-selenium/test-output/*/*/*/*/debug/projects/harness.log ~/zm-selenium/test-output
+
+# Print process output to check java memory use
+top -n 1 -b > ~/zm-selenium/process-output.txt
